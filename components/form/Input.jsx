704f4cab7f4e8f3bda67a736262fbfbae5ded6c7
Osman ÖZBEK <77728642+ozbek2o@users.noreply.github.com>
2023-05-25 00:28:51 +0300

    input components

--- components/form/Input.jsx
+++ components/form/Input.jsx
@@ -4,8 +4,10 @@ const Input = () => {
   return (
     <div className='w-full'>
          <label className='relative block'>
-            <input type="text" className='h-14 w-full border-primary outline-none'/>
-            <span>Email</span>
+            <input type="text"  className='h-14 w-full border-primary 
+            outline-none px-4 peer pt-2' required/>
+            <span className='absolute top-0 left-0 px-4 text-sm flex 
+            h-full peer-focus:h-7 peer-focus:text-xs peer-valid:h-7 peer-valid:text-xs transition-all'>Email</span>
          </label>
     </div>
   );
