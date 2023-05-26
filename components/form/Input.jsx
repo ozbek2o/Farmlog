062d074d00804ccb87b67cb287f40b38476408df
Osman ÖZBEK <77728642+ozbek2o@users.noreply.github.com>
2023-05-26 18:32:36 +0300

    email.

--- components/form/Input.jsx
+++ components/form/Input.jsx
@@ -7,7 +7,7 @@ const Input = () => {
             <input type="text"  className='h-14 w-full border-primary 
             outline-none px-4 peer pt-2' required/>
             <span className='absolute top-0 left-0 px-4 text-sm flex 
-            h-full peer-focus:h-7 peer-focus:text-xs peer-valid:h-7 peer-valid:text-xs transition-all'>Email</span>
+            h-full peer-focus:h-7 peer-focus:text-xs peer-valid:h-7 peer-valid:text-xs transition-all'>E-maiil</span>
          </label>
     </div>
   );
