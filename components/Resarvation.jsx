65b1955ad107a12a4b8af30dcfb369201981342f
Osman ÖZBEK <77728642+ozbek2o@users.noreply.github.com>
2023-05-30 23:19:31 +0300

    reservation2

--- components/Resarvation.jsx
+++ components/Resarvation.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
-
+import Title from './ui/Title'
+import Input from './form/Input'
 const Resarvation = () => {
   return (
-    <div className='container mx-auto py-6 '> Resarvation</div>
+    <div className='container mx-auto py-12 '> 
+    <div>
+        <Title addClass="text-[40px]">Book A Table</Title>
+    </div>
+    <div>
+        <Input/>
+    </div>
+    <div></div>
+    </div>
   )
 }
 
