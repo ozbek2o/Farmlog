9cfa518442f3726df3fa145e9a2e7e6aad900580
Osman ÖZBEK <77728642+ozbek2o@users.noreply.github.com>
2023-06-06 23:53:30 +0300

    div

--- components/Resarvation.jsx
+++ components/Resarvation.jsx
@@ -20,8 +20,10 @@ const Resarvation = () => {
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d784910.1328813714!2d36.331034712207966!3d39.78331643449517!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x407eaa28129f0613%3A0xe2f2fcefa3ddaa2b!2sSivas%20Merkez%2FSivas!5e0!3m2!1str!2str!4v1685970636963!5m2!1str!2str" width="600" height="450"  
         allowfullScreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" className='w-full h-full'></iframe>
         </div>
+        <div></div>
     </div>
     </div>
+    
     
   )
 }
