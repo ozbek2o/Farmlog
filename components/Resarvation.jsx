eb75e1149ce4b0314944f7d41fabeecb864e9b27
Osman ÖZBEK <77728642+ozbek2o@users.noreply.github.com>
2023-06-13 01:39:51 +0300

    const

--- components/Resarvation.jsx
+++ components/Resarvation.jsx
@@ -2,17 +2,54 @@ import React from 'react'
 import Title from './ui/Title'
 import Input from './form/Input'
 const Resarvation = () => {
+
+    const inputs =[
+        {
+            id:1,
+            name:"fullName",
+            typr:"text",
+            placeholder:"Your Full Name",
+
+        },
+        {
+            id:2,
+            name:"phoneNumber",
+            type:"text",
+            placeholder:"Your phone Name",
+            
+        },
+        {
+            id:3,
+            name:"email",
+            type:"text",
+            placeholder:"Email addres",
+            
+        },
+        {
+            id:3,
+            name:"persons",
+            type:"number",
+            placeholder:"how money person",
+            
+        },
+        {
+            id:4,
+            name:"persons",
+            type:"date",
+            placeholder:"how money person",
+            
+        },
+    ];
+
   return (
     <div className='container mx-auto py-12 '> 
     <Title addClass="text-[40px] mb-10">Book A Table</Title> 
     <div className='flex justify-between flex-wrap items-center gap-x-10'>
        <div className='lg:flex-1 w-full'>
     <div className='flex flex-col gap-y-4'>
-        <Input/>
-        <Input/>
-        <Input/>
-        <Input/>
-        <Input/>
+      {inputs.map((input)=>(
+        <input key={input.id} {...input}/>
+      ))}
         </div>
         <button className="btn-primary mt-4">BOOK NOW</button>
         </div>
@@ -20,7 +57,7 @@ const Resarvation = () => {
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d784910.1328813714!2d36.331034712207966!3d39.78331643449517!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x407eaa28129f0613%3A0xe2f2fcefa3ddaa2b!2sSivas%20Merkez%2FSivas!5e0!3m2!1str!2str!4v1685970636963!5m2!1str!2str" width="600" height="450"  
         allowfullScreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" className='w-full h-full'></iframe>
         </div>
-        <div></div>
+        
     </div>
     </div>
     
--- components/form/Input.jsx
+++ components/form/Input.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 
-const Input = () => {
+const Input = (props) => {
+  const {placeholder, ...inputProps}=props;
   return (
     <div className='w-full'>
          <label className='relative block'>
             <input type="text"  className='h-14 w-full border-primary 
-            outline-none px-4 peer pt-2' required/>
+            outline-none px-4 peer pt-2' required {...inputProps}/>
             <span className='absolute top-0 left-0 px-4 text-sm flex 
-            h-full peer-focus:h-7 peer-focus:text-xs peer-valid:h-7 peer-valid:text-xs transition-all'>E-maiil</span>
+            h-full peer-focus:h-7 peer-focus:text-xs peer-valid:h-7 peer-valid:text-xs transition-all'>{placeholder}</span>
          </label>
     </div>
     
